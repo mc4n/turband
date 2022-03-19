ddc0f4665545fd356b0b9a07bbb64792958049d2
Mustaph John <mustcean@gmail.com>
2022-03-19 12:08:27 +0300

    admin panel restricted

--- routes/web.php
+++ routes/web.php
@@ -3,26 +3,22 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'App\Http\Controllers\HomeController@index')->name("home.index");
-
 Route::get('/about', 'App\Http\Controllers\HomeController@about')->name("home.about");
 
 Route::get('/define', 'App\Http\Controllers\DefineController@search')->name("define.search");
-
 Route::get('/add', 'App\Http\Controllers\DefineController@add')->name("define.add");
-
 Route::post('/add', 'App\Http\Controllers\DefineController@add_post')->name("define.add_post");
 
+Route::middleware('admin')->group(function () {
 
-Route::get('/admin', 'App\Http\Controllers\Admin\AdminHomeController@index')->name("admin.home.index");
+	Route::get('/admin', 'App\Http\Controllers\Admin\AdminHomeController@index')->name("admin.home.index");
+	Route::get('/admin/words', 'App\Http\Controllers\Admin\AdminWordController@index')->name("admin.word.index");
+	Route::post('/admin/words/store', 'App\Http\Controllers\Admin\AdminWordController@store')->name("admin.word.store");
 
-Route::get('/admin/words', 'App\Http\Controllers\Admin\AdminWordController@index')->name("admin.word.index");
+	Route::delete('/admin/words/{id}/delete', 'App\Http\Controllers\Admin\AdminWordController@delete')->name("admin.word.delete");
+	Route::get('/admin/word/{id}/edit', 'App\Http\Controllers\Admin\AdminWordController@edit')->name("admin.word.edit");
+	Route::put('/admin/word/{id}/update', 'App\Http\Controllers\Admin\AdminWordController@update')->name("admin.word.update");
+});
 
-Route::post('/admin/words/store', 'App\Http\Controllers\Admin\AdminWordController@store')->name("admin.word.store");
-
-Route::delete('/admin/words/{id}/delete', 'App\Http\Controllers\Admin\AdminWordController@delete')->name("admin.word.delete");
-
-Route::get('/admin/word/{id}/edit', 'App\Http\Controllers\Admin\AdminWordController@edit')->name("admin.word.edit");
-
-Route::put('/admin/word/{id}/update', 'App\Http\Controllers\Admin\AdminWordController@update')->name("admin.word.update");
 
 Auth::routes();
